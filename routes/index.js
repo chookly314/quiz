030e0cc802753fd827cb7fade47271dd38506da1
fcepeda <fernando-cepso@outlook.com>
2015-05-10 12:41:10 +0200

    Validación

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,16 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+//Página de autores
+router.get('/author', function(req, res) {
+  res.render('author', { errors: []});
+});
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -17,8 +22,5 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
-router.get('/author',  function(req, res) {
-  res.render('author');
-});
 
 module.exports = router;
